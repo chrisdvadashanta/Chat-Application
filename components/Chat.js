88380b80f892f055d576be99303da765c9201a7e
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-08-15 14:33:39 +0200

    adding gifted chat with state, useEffect and Keyb.Avoid.vie

--- components/Chat.js
+++ components/Chat.js
@@ -1,40 +1,75 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useEffect, useState } from "react";
+import { StyleSheet, View, KeyboardAvoidingView } from "react-native";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
-    const { name, backgroundColor } = route.params;
-    const containerStyle = { ...styles.container, backgroundColor: backgroundColor };
+  const [messages, setMessages] = useState([]);
+  const { name, backgroundColor } = route.params;
+  
+  useEffect(() => {
+    navigation.setOptions({ title: name });
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
+ 
+  const containerStyle = {
+    ...styles.container,
+    backgroundColor: backgroundColor,
+  };
 
-    useEffect(() => {
-        navigation.setOptions({ title: name });
-      }, []);
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
 
- return (
-   <View style={containerStyle}>
-      <View style={styles.textContainer}>
-          <Text>Hello Chat!</Text>
-      </View>     
-   </View>
- );
-}
+  const renderBubble = (props) => {
+    console.log("Rendering bubble...");
+    return ( <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "black"
+        },
+        left: {
+          backgroundColor: "#FFF"
+        }
+      }}
+    />
+    );
+  };
+
+  return (
+    <View style={containerStyle}>
+      <KeyboardAvoidingView 
+        behavior={Platform.OS === "ios" ? "padding" : null} 
+        style={{ flex: 1 }} // This ensures it takes up all available space
+        keyboardVerticalOffset={Platform.OS === "ios" ? 0 : -300} // Adjust this value as needed
+      >
+      <GiftedChat
+      style= {styles.GiftedChat}
+      messages={messages}
+      renderBubble={renderBubble}
+      onSend={messages => onSend(messages)}
+      user={{ _id: 1 }}
+    />
+    </KeyboardAvoidingView>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',   //align the child centered
   },
-  textContainer: {
-    paddingHorizontal: 10,   // Add some horizontal padding
-    paddingVertical: 5, 
-    borderRadius: 5,
-    alignSelf: 'center',
-    backgroundColor: '#ffd966',
-    width: '50%',
-    height: 40,
-    justifyContent: 'center',  // Centers text vertically inside the view.
-    alignItems: 'center',      // Centers text horizontally inside the view.
-  }
 });
 
 export default Chat;
