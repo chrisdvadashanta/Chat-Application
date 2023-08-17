e387152cdaa7b42c63dd86fab5d58e5dc29d154e
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-08-17 15:49:14 +0200

    including firebase and passing it to Chat Component

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 import Start from './components/Start';
 import Chat from './components/Chat';
@@ -10,6 +12,21 @@ const Stack = createNativeStackNavigator();
 
 
 export default function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyAw6p0J8sb2SxQNuc_xumHEncIwD4rUYOI",
+    authDomain: "chat-app-fb591.firebaseapp.com",
+    projectId: "chat-app-fb591",
+    storageBucket: "chat-app-fb591.appspot.com",
+    messagingSenderId: "57868372876",
+    appId: "1:57868372876:web:efcf57f92e3e5c4b46a4a9"
+    };
+    // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
+    
   return (
     // Naviagtion between different screens
     <NavigationContainer>       
@@ -23,7 +40,7 @@ export default function App() {
         />
         <Stack.Screen
           name="Chat"
-          component={Chat}
+          children={(props) => <Chat db={db} {...props} />}
         />
       </Stack.Navigator>
     
