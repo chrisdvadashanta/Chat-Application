88ff56b04fc1b166b9d8302a9c5b959930fb9806
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-08-18 12:30:46 +0200

    including useNetInfo and .isConnected

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import { useNetInfo } from "@react-native-community/netinfo";
+import { useEffect } from "react";
+import { LogBox, Alert } from "react-native";
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore , disableNetwork, enableNetwork } from "firebase/firestore";
+
 
 import Start from './components/Start';
 import Chat from './components/Chat';
@@ -12,6 +16,8 @@ const Stack = createNativeStackNavigator();
 
 
 export default function App() {
+
+  ///firebase Code
   const firebaseConfig = {
     apiKey: "AIzaSyAw6p0J8sb2SxQNuc_xumHEncIwD4rUYOI",
     authDomain: "chat-app-fb591.firebaseapp.com",
@@ -20,11 +26,19 @@ export default function App() {
     messagingSenderId: "57868372876",
     appId: "1:57868372876:web:efcf57f92e3e5c4b46a4a9"
     };
-    // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-
-  // Initialize Cloud Firestore and get a reference to the service
   const db = getFirestore(app);
+
+  /// Connection
+  const connectionStatus = useNetInfo();
+  useEffect(() => {
+    if (connectionStatus.isConnected === false)
+    { Alert.alert("Connection lost!");
+    disableNetwork(db);
+  } else if (connectionStatus.isConnected === true) {
+    enableNetwork(db);
+  }
+}, [connectionStatus.isConnected]);
 
     
   return (
@@ -40,7 +54,7 @@ export default function App() {
         />
         <Stack.Screen
           name="Chat"
-          children={(props) => <Chat db={db} {...props} />}
+          children={props => <Chat isConnected={connectionStatus.isConnected} db={db} {...props}/>}
         />
       </Stack.Navigator>
     
